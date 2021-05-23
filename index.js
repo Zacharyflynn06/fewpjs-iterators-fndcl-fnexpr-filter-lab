bf948339e67d131b8507d17b3ebb757765951cd9
Zac <zacharyflynn06@gmail.com>
2021-05-23 13:51:55 -0600

    commit tree 3233199dbc639fdd7b3bb4f316ff2b432b3efeb3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(collection, id) {
+    return collection.filter( result => 
+        result.toLowerCase() === id.toLowerCase()
+    )
+}
+
+function fuzzyMatch(collection, id) {
+    return collection.filter( result => 
+        result.toLowerCase().indexOf(id.toLowerCase()) === 0
+    )
+}
+
+function matchName(collection, id) {
+    return collection.filter( data => data.name === id)
+}
